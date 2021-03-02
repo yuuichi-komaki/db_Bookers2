70ba3e3fa8e1d5c20a2f4400e95953b01e6225c2
yuuichi-komaki <yuuichi.k.1109@gmail.com>
2021-03-02 09:17:36 +0000

    finish

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+   devise_for :users
+  
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+ 
+  resources :users, only: [:show, :index, :edit, :update, :destroy]
+  resources :books
+  
+ 
+ end
